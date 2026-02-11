1cda71b9a0e1ad8b835b614577d1668aa902ee48
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-11 17:33:41 +0200

    added scripts for automation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime as dt
 import smtplib
 import dotenv
 import os
+import time
 
 
 dotenv.load_dotenv()
@@ -13,43 +14,60 @@ MY_LONG = 28.047304
 ENDPOINT = "https://api.sunrise-sunset.org/json"
 
 
+def check():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-
-parameter = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0 
-}
-
-response = requests.get(ENDPOINT, params=parameter)
-response.raise_for_status()
-data = response.json()
-# print(data)
-
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-
-def check(current_iss_la, current_iss_lng):
-    print("current iss :",current_iss_la,current_iss_lng)
-    print("my lat and lng:",MY_LAT, MY_LONG)
-    if (current_iss_la >= MY_LAT-5 and current_iss_la <= MY_LAT+5) and (current_iss_lng >= MY_LONG-5 and current_iss_lng <= MY_LONG +5):
+    if (iss_latitude >= MY_LAT-5 and iss_latitude <= MY_LAT+5) and (iss_longitude >= MY_LONG-5 and iss_longitude <= MY_LONG +5):
         return True
-    
-now_time = dt.datetime.now().hour
-# print(type(now_time))
-if check(iss_latitude, iss_longitude):
-    # check if now is under sunrise and not oversunrise and go see the iss pass
 
+
+def check_night():
+    parameter = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0 
+    }
+
+    response = requests.get(ENDPOINT, params=parameter)
+    response.raise_for_status()
+    data = response.json()
+    # print(data)
+
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    now_time = dt.datetime.now().hour
     if now_time >= sunrise and now_time <= sunset:
-        
-        with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login()
+         return True
+
+    
+
+# print(type(now_time))
+send = False
+while not send:
+     
+    time.sleep(5)
+    if check() and check_night():
+        # check if now is under sunrise and not oversunrise and go see the iss pass
+            with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+
+                message = "Go outside to see the ISS pass on your current location \n Look up\nBy\nNjabs" 
+                connection.sendmail(
+                    from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject: ISS passing \n\n{message}"
+                )
+
+            send = True
+
+
+
+    else:print("none")
+#     else:
+#         print("its not the write time to go outside")
+# else:
+#     print("its not there yet")
